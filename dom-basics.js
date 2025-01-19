2115cbc91889c09abfee3c8fd76600247d817456
cconnorhobbs <150186838+cconnorhobbs@users.noreply.github.com>
2025-01-18 17:44:43 -0700

    hw

--- dom-basics.js
+++ dom-basics.js
@@ -2,6 +2,7 @@ const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
 
+
 const image = document.createElement('img');
 image.setAttribute('src', 'https://picsum.photos/200');
 image.setAttribute('src', 'random image');
@@ -10,3 +11,7 @@ document.body.appendChild(image);
 const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
+
+const newSection = document.createElement("Section 3");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(newSection);
