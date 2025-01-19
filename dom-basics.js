7239792d080c53adc16edfe3c1bf294d4a234d37
cconnorhobbs <150186838+cconnorhobbs@users.noreply.github.com>
2025-01-18 17:45:26 -0700

    hw

--- dom-basics.js
+++ dom-basics.js
@@ -13,5 +13,5 @@ newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
 
 const newSection = document.createElement("Section 3");
-newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+newSection.innerHTML = "<h2>DOM Basics</h2><p>DOM basics for section 3 h2 of part 4</p>";
 document.body.appendChild(newSection);
