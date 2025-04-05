4d907165ab1c1aa76edc1385d211a98d774bfd6d
cconnorhobbs <150186838+cconnorhobbs@users.noreply.github.com>
2025-04-04 23:44:04 -0600

    hw

--- personalsite/map.js
+++ personalsite/map.js
@@ -4,3 +4,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([43.81961213605789, -111.78608555070839]).addTo(map);
+
+var marker = L.marker([43.81861184137616, -111.78551175111855]).addTo(map);
+
+var marker = L.marker([43.819422687186524, -111.78704451374344]).addTo(map);
+
+var marker = L.marker([43.822336610891256, -111.79103634565419]).addTo(map);
+
+var marker = L.marker([43.832960116396805, -111.8012603297652]).addTo(map);
+
+var marker = L.marker([43.826720424137186, -111.7745654591547]).addTo(map);
+
+var marker = L.marker([43.83092093904061, -111.77133229580086]).addTo(map);
+
